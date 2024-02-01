621105f54f7a741d48412eeb3cb5bba56ee80d19
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 21:38:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, Response, render_template
+from urllib.request import urlopen
+
+app = Flask(__name__)
+
+user_site = ""
+
+@app.route('/clone/<path:site>')
+def set_site(site):
+    global user_site
+    user_site = site
+    return f"User site set to: {user_site}"
+
+@app.route('/<path:url>')
+def proxy(url):
+    global user_site
+    full_url = user_site + '/' + url
+    try:
+        response = urlopen(full_url)
+        content_type = response.getheader('Content-Type')
+        html_content = response.read()
+
+        return Response(html_content, content_type=content_type)
+    except Exception as e:
+        return str(e)
+
+if __name__ == '__main__':
+    app.run(debug=True)
