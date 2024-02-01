ede8067202b4023dbb4436b471d92d7d74579bf0
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-02 00:29:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,19 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9',
 }
-js_code = """
+
+js_code = f"""
 <script>
-    document.addEventListener('DOMContentLoaded', () => {
-        document.body.addEventListener('click', e => {
+    document.addEventListener('DOMContentLoaded', () => {{
+        document.body.addEventListener('click', e => {{
             const targetUrl = e.target.getAttribute('href');
-            if (targetUrl && targetUrl.startsWith('http') && !window.confirm('You are leaving the proxy site and going to ' + targetUrl + '. Continue?')) {
+            const isExternalLink = targetUrl && targetUrl.startsWith('http') && !targetUrl.startsWith({server_site});
+            
+            if (isExternalLink && !window.confirm('You are leaving the proxy site and going to ' + targetUrl + '. Continue?')) {{
                 e.preventDefault();
-            }
-        });
-    });
+            }}
+        }});
+    }});
 </script>
 """
 
