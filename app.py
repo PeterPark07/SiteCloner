9b49d70c749265213c9c429e9833307276a5c2c0
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-02 01:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 from flask import Flask, request, Response
-import requests 
+import requests
 import os
 
 app = Flask(__name__)
 server_url = os.getenv('url')
 
 user_site = ""
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.9',
-}
-
-js_code = f"""
+js_code = """
 <script>
-    document.addEventListener('DOMContentLoaded', () => {{
-        document.body.addEventListener('click', e => {{
+    document.addEventListener('DOMContentLoaded', () => {
+        document.body.addEventListener('click', e => {
             const targetUrl = e.target.getAttribute('href');
             const isExternalLink = targetUrl && targetUrl.startsWith('http') && !targetUrl.startsWith({server_url});
             
-            if (isExternalLink && !window.confirm('You are leaving the proxy site and going to ' + targetUrl + '. Continue?')) {{
+            if (isExternalLink && !window.confirm('You are leaving the proxy site and going to ' + targetUrl + '. Continue?')) {
                 e.preventDefault();
-            }}
-        }});
-    }});
+            }
+        });
+    });
 </script>
 """
 
+# Create a session and set headers
 session = requests.Session()
+session.headers.update({
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+})
 
 @app.route('/clone', methods=['GET', 'POST'])
 def clone_site():
@@ -47,7 +47,7 @@ def fetch_and_modify_content(url):
     global user_site
     full_url = user_site + '/' + url
     try:
-        response = session.get(full_url, headers=headers)
+        response = session.get(full_url)
         content_type = response.headers['Content-Type']
         html_content = response.content.replace(user_site.encode('utf-8'), server_url.encode('utf-8'))
         return html_content.replace(b'</head>', js_code.encode('utf-8') + b'</head>', 1), content_type
