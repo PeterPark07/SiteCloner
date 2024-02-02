1876f576801d325c3f40dcf0c44e5c6fd10af4f9
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-02 12:20:27 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,19 @@
+hearders = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+js_code = """
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        document.body.addEventListener('click', e => {
+            const targetUrl = e.target.getAttribute('href');
+            const isExternalLink = targetUrl && targetUrl.startsWith('http') && !targetUrl.startsWith({server_url});
+            
+            if (isExternalLink && !window.confirm('You are leaving the proxy site and going to ' + targetUrl + '. Continue?')) {
+                e.preventDefault();
+            }
+        });
+    });
+</script>
+"""
