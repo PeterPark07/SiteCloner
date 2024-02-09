d2730d8957feb0526b597097838610417baefd22
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-10 01:06:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,12 +53,13 @@ Visited URLs:
 
 def fetch_and_modify_content(url):
     full_url = user_site + '/' + url
+    user_sitename = user_site.replace('https://','')
     try:
         response = session.get(full_url)
         content_type = response.headers['Content-Type']
 
         if content_type.startswith(('text', 'application')):
-            html_content = response.content.replace(user_site.encode('utf-8'), server_url.encode('utf-8'))
+            html_content = response.content.replace(user_sitename.encode('utf-8'), server_url.encode('utf-8'))
             html_content = html_content.replace(b'</head>', js_code.encode('utf-8') + b'</head>', 1)
             return html_content, content_type
             
